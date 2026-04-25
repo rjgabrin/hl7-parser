22eefcd9420c9b9c580939e64a5120653f2a0c35
Ryan Gabrin <36611105+rjgabrin@users.noreply.github.com>
2026-04-25 15:43:56 -0400

    Create OrderRecord.ts

---
+++ HL7TestApp-TypeScript/OrderRecord.ts
@@ -0,0 +1,10 @@
+export interface OrderRecord {
+    SendingApplication: string | null;
+    MessageType: string | null;
+    PatientId: string | null;
+    PatientName: string | null;
+    OrderControlCode: string | null;
+    OrderId: string | null;
+    UniversalServiceId: string | null;
+    OrderedDateTime: string | null;
+}
